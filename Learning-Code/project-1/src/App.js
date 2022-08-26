3e3a0e682f78bab9bf43fc19179a87c16e30cda7
Jay <jay.sanjay.karia@gmail.com>
2022-08-26 19:44:26 +0530

    alert features added

--- Learning-Code/project-1/src/App.js
+++ Learning-Code/project-1/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Alert from './Components/Alert'
 import Navbar from './Components/Navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/About';
@@ -6,21 +7,39 @@ import {useState} from "react";
 
 function App() {
     const [mode, setMode] = useState("light");
+
+    const [alert, setAlert] = useState(null)
+
+    const showAlert = (message, type)=> {
+        setAlert(
+            {
+                msg:message,
+                type:type
+            }
+        )
+        setTimeout(()=> {
+            setAlert(null)
+        }, 3000)
+    }
+
     const toggleMode = () => {
         if (mode === "dark") {
             setMode("light")
             document.getElementsByTagName('body')[0].style.backgroundColor="white"
             document.getElementsByTagName('body')[0].style.color="black"
+            showAlert("Light Mode has been enabled", "success")
         } else {
             setMode("dark")
-            document.getElementsByTagName('body')[0].style.backgroundColor="hsl(200, 100%, 20%)"
+            document.getElementsByTagName('body')[0].style.backgroundColor="hsl(200, 100%, 30%)"
             document.getElementsByTagName('body')[0].style.color="white"
+            showAlert("Dark Mode has been enabled", "success")
         }
     }
     return (
         <>
             <Navbar title="TextUtils" aboutText="About TextUtils" mode={mode} toggleMode={toggleMode}/>
-            <TextForm heading="Enter text to analyze below" mode={mode}/>
+            <Alert alert={alert}/>
+            <TextForm heading="Enter text to analyze below" mode={mode} showAlert = {showAlert}/>
             {/* <About/> */}
         </>
 
--- Learning-Code/project-1/src/Components/TextForm.js
+++ Learning-Code/project-1/src/Components/TextForm.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, {useState} from 'react';
 
 
 export default function TextForm(props) {
     const handleUpClick = () => {
         const newText = text.toUpperCase()
         setText(newText)
+        props.showAlert("Converted to Uppercase", "success")
     }
 
     const handleOnChange = (event) => {
@@ -14,22 +15,26 @@ export default function TextForm(props) {
     const handleDownClick = () => {
         const downText = text.toLowerCase()
         setText(downText)
+        props.showAlert("Converted to Lowercase", "success")
     }
 
-    const clear = ()=> {
+    const clear = () => {
         setText("")
+        props.showAlert("Text cleared", "danger")
     }
 
-    const handleCopy = ()=> {
+    const handleCopy = () => {
         let text = document.getElementById('exampleFormControlTextarea1');
         text.select()
         navigator.clipboard.writeText(text.value)
+        props.showAlert("Text Copied", "success")
     }
 
-    const handleTrim = ()=> {
+    const handleTrim = () => {
         let text = document.getElementById('exampleFormControlTextarea1').value
         let newText = text.split(/[ ]+/)
         setText(newText.join(" "))
+        props.showAlert("Text is trimmed", "info")
     }
 
     const [text, setText] = useState("")
@@ -45,7 +50,10 @@ export default function TextForm(props) {
                     rows={8}
                     value={text}
                     onChange={handleOnChange}
-                    style={{backgroundColor: props.mode==="dark"? "grey":"white"}}
+                    style={{
+                        backgroundColor: props.mode === "dark" ? "grey" : "white",
+                        color: props.mode === "dark" ? "white" : "black"
+                    }}
                 />
                 <button className="btn btn-dark my-3 mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
                 <button className="btn btn-secondary my-3 mx-2" onClick={handleDownClick}>Convert To LowerCase</button>
@@ -57,17 +65,21 @@ export default function TextForm(props) {
             <div className="container my-3">
                 <h1>Your Text Summary</h1>
                 <h5 className="my-2">
-                    Total Words: <span style={{ "color": "green", "font-weight": "bolder" }}>{text.split(" ").length}</span>
+                    Total Words: <span
+                    style={{"color": "green", "font-weight": "bolder"}}>{text.split(" ").length}</span>
                 </h5>
                 <h5 className="my-2">
-                    Total Characters: <span style={{ "color": "green", "font-weight": "bolder" }}>{text.length}</span>
+                    Total Characters: <span style={{"color": "green", "font-weight": "bolder"}}>{text.length}</span>
                 </h5>
                 <h5 className="my-2">
-                    <span style={{ "color": "green", "font-weight": "bolder" }}>{((text.length)*0.008).toFixed(2)} Minutes</span> To Read
+                    <span style={{
+                        "color": "green",
+                        "font-weight": "bolder"
+                    }}>{((text.length) * 0.008).toFixed(2)} Minutes</span> To Read
                 </h5>
-                <br />
+                <br/>
                 <h1>Preview</h1>
-                <p>{text.length<=0? "Enter something to get the Preview": text}</p>
+                <p>{text.length <= 0 ? "Enter something to get the Preview" : text}</p>
             </div>
         </>
 
