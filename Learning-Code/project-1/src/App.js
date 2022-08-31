e6a0f683b6b3d42f113afe887f1a13e5b3b85378
Jay <jay.sanjay.karia@gmail.com>
2022-08-31 09:39:26 +0530

    all themes completed

--- Learning-Code/project-1/src/App.js
+++ Learning-Code/project-1/src/App.js
@@ -3,48 +3,60 @@ import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import About from "./Components/About";
-import { useState } from "react";
+import {useState} from "react";
 
 function App() {
-  const [alert, setAlert] = useState(null);
+    const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) => {
-    setAlert({
-      msg: message,
-      type: type,
-    });
-    setTimeout(() => {
-      setAlert(null);
-    }, 3000);
-  };
+    const showAlert = (message, type) => {
+        setAlert({
+            msg: message,
+            type: type,
+        });
+        setTimeout(() => {
+            setAlert(null);
+        }, 3000);
+    };
 
-  //   const [theme, setTheme] = useState('light')
-  const applyTheme = (bg, font, special, button, cls) => {
-    console.log(bg);
-    document.getElementsByTagName("body")[0].style.backgroundColor = bg;
-    document.getElementsByTagName("body")[0].style.color = font;
-    for (let i = 0; i < 3; i++)
-      document.getElementsByClassName("sp")[i].style.color = special;
-    document.getElementsByTagName("textarea")[0].style.backgroundColor =
-      special;
-    document.getElementsByTagName("nav")[0].style.color = font;
-    for (let j=0;j<7;j++)
-    document.getElementsByTagName("button")[j].style.backgroundColor = button;
+    const removeGlow = ()=> {
+        document.getElementsByClassName('bg-primary')[0].style.boxShadow = '0 0 0 0 white'
+        document.getElementsByClassName('bg-warning')[0].style.boxShadow = '0 0 0 0 white'
+        document.getElementsByClassName('bg-success')[0].style.boxShadow = '0 0 0 0 white'
+        document.getElementsByClassName('bg-danger')[0].style.boxShadow = '0 0 0 0 white'
+        document.getElementsByClassName('bg-light')[0].style.boxShadow = '0 0 0 0 white'
+        document.getElementsByClassName('bg-dark')[0].style.boxShadow = '0 0 0 0 white'
+    }
 
-    showAlert('Theme changed', cls)
-  };
+    //   const [theme, setTheme] = useState('light')
+    const applyTheme = (bg, font, special, button, cls, textareaColor, buttonFont, textareaFont) => {
+        removeGlow()
+        document.getElementsByTagName("body")[0].style.backgroundColor = bg;
+        document.getElementsByTagName("body")[0].style.color = font;
+        for (let i = 0; i < 3; i++)
+            document.getElementsByClassName("sp")[i].style.color = special;
+        document.getElementsByTagName("textarea")[0].style.backgroundColor = textareaColor;
+        document.getElementsByTagName("textarea")[0].style.color = textareaFont;
+        document.getElementsByTagName("nav")[0].style.color = font;
+        for (let j = 0; j < 7; j++) {
+            document.getElementsByTagName("button")[j].style.backgroundColor = button;
+            document.getElementsByTagName("button")[j].style.color = buttonFont;
+        }
+        document.getElementsByClassName('bg-'+cls)[0].style.boxShadow = `0 0 5px 5px ${bg}`
 
-  return (
-    <>
-      <Navbar
-        title="TextUtils"
-        aboutText="About TextUtils"
-        applyTheme={applyTheme}
-      />
-      <Alert alert={alert} />
-      <TextForm heading="Enter text to analyze below" showAlert={showAlert} />
-    </>
-  );
+        showAlert("Theme changed", cls);
+    };
+
+    return (
+        <>
+            <Navbar
+                title="TextUtils"
+                aboutText="About TextUtils"
+                applyTheme={applyTheme}
+            />
+            <Alert alert={alert}/>
+            <TextForm heading="Enter text to analyze below" showAlert={showAlert}/>
+        </>
+    );
 }
 
 export default App;
--- Learning-Code/project-1/src/Components/Navbar.js
+++ Learning-Code/project-1/src/Components/Navbar.js
@@ -37,27 +37,27 @@ export default function Navbar(props) {
         </div>
         <div className="d-flex">
           <div
-            className="bg-primary rounded mx-2" onClick={()=>{props.applyTheme('hsl(200, 82%, 35%)', 'white', 'hsl(179, 53%, 70%)', 'blue', 'primary')}}
+            className="bg-primary rounded mx-2" onClick={()=>{props.applyTheme('hsl(200, 82%, 35%)', 'white', 'hsl(179, 53%, 1%)', 'blue', 'primary', 'hsl(179, 53%, 70%)', 'white', 'black')}}
             style={{ height: "30px", aspectRatio: "1" }}
           ></div>
           <div
-            className="bg-warning rounded mx-2"
+            className="bg-warning rounded mx-2" onClick={()=>{props.applyTheme('hsl(50, 72%, 45%)', 'black', 'hsl(57, 63%, 1%)', 'yellow', 'warning','hsl(57, 63%, 80%)', 'black', 'black')}}
             style={{ height: "30px", aspectRatio: "1" }}
           ></div>
           <div
-            className="bg-success rounded mx-2"
+            className="bg-success rounded mx-2" onClick={()=>{props.applyTheme('hsl(150, 82%, 35%)', 'white', 'hsl(140, 53%, 1%)', 'green', 'success', 'hsl(140, 53%, 70%)', 'white', 'black')}}
             style={{ height: "30px", aspectRatio: "1" }}
           ></div>
           <div
-            className="bg-light rounded mx-2"
+            className="bg-light rounded mx-2" onClick={()=>{props.applyTheme('white', 'black', 'green', 'black', 'light', 'hsl(0, 5%, 90%)', 'white', 'black')}}
             style={{ height: "30px", aspectRatio: "1"}}
           ></div>
           <div
-            className="bg-dark rounded mx-2"
+            className="bg-dark rounded mx-2" onClick={()=>{props.applyTheme('black', 'white', 'green', 'white', 'dark', 'hsl(0, 5%, 10%)', 'black', 'white')}}
             style={{ height: "30px", aspectRatio: "1" , border:'2px solid white'}}
           ></div>
           <div
-            className="bg-danger rounded mx-2"
+            className="bg-danger rounded mx-2" onClick={()=>{props.applyTheme('hsl(5, 50%, 30%)', 'white', 'hsl(57, 63%, 1%)', 'red', 'danger', 'hsl(4,27%,68%)', 'white', 'black')}}
             style={{ height: "30px", aspectRatio: "1"}}
           ></div>
         </div>
