eaf61484557a95ce68543a215d30c7d330921959
Jay <jay.sanjay.karia@gmail.com>
2022-08-31 21:08:57 +0530

    about component themes bug remaining

--- Learning-Code/project-1/src/App.js
+++ Learning-Code/project-1/src/App.js
@@ -3,60 +3,79 @@ import Alert from "./Components/Alert";
 import Navbar from "./Components/Navbar";
 import TextForm from "./Components/TextForm";
 import About from "./Components/About";
-import {useState} from "react";
+import { useState } from "react";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    const [alert, setAlert] = useState(null);
+  const [alert, setAlert] = useState(null);
+  // const [theme, setTheme] = useState({
+  //   'bodyColor':'white'
+  // })
 
-    const showAlert = (message, type) => {
-        setAlert({
-            msg: message,
-            type: type,
-        });
-        setTimeout(() => {
-            setAlert(null);
-        }, 3000);
-    };
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  };
 
-    const removeGlow = ()=> {
-        document.getElementsByClassName('bg-primary')[0].style.boxShadow = '0 0 0 0 white'
-        document.getElementsByClassName('bg-warning')[0].style.boxShadow = '0 0 0 0 white'
-        document.getElementsByClassName('bg-success')[0].style.boxShadow = '0 0 0 0 white'
-        document.getElementsByClassName('bg-danger')[0].style.boxShadow = '0 0 0 0 white'
-        document.getElementsByClassName('bg-light')[0].style.boxShadow = '0 0 0 0 white'
-        document.getElementsByClassName('bg-dark')[0].style.boxShadow = '0 0 0 0 white'
-    }
+  const [theme, setTheme] = useState({bg:'green', otherColor:'lightgreen', anotherColor: 'hsl(150, 82%, 35%)', color:'white'})
 
-    //   const [theme, setTheme] = useState('light')
-    const applyTheme = (bg, font, special, button, cls, textareaColor, buttonFont, textareaFont) => {
-        removeGlow()
-        document.getElementsByTagName("body")[0].style.backgroundColor = bg;
-        document.getElementsByTagName("body")[0].style.color = font;
-        for (let i = 0; i < 3; i++)
-            document.getElementsByClassName("sp")[i].style.color = special;
-        document.getElementsByTagName("textarea")[0].style.backgroundColor = textareaColor;
-        document.getElementsByTagName("textarea")[0].style.color = textareaFont;
-        document.getElementsByTagName("nav")[0].style.color = font;
-        for (let j = 0; j < 6; j++) {
-            document.getElementsByTagName("button")[j].style.backgroundColor = button;
-            document.getElementsByTagName("button")[j].style.color = buttonFont;
-        }
-        document.getElementsByClassName('bg-'+cls)[0].style.boxShadow = `0 0 5px 5px ${bg}`
+  const removeGlow = () => {
+    document.getElementsByClassName("bg-primary")[0].style.boxShadow = "0 0 0 0 white";
+    document.getElementsByClassName("bg-warning")[0].style.boxShadow = "0 0 0 0 white";
+    document.getElementsByClassName("bg-success")[0].style.boxShadow = "0 0 0 0 white";
+    document.getElementsByClassName("bg-danger")[0].style.boxShadow = "0 0 0 0 white";
+    document.getElementsByClassName("bg-light")[0].style.boxShadow = "0 0 0 0 white";
+    document.getElementsByClassName("bg-dark")[0].style.boxShadow = "0 0 0 0 white";
+  };
+  const applyTheme = (bg,font,special,button,cls,textareaColor,buttonFont,textareaFont) => {
+    removeGlow();
+    try {
+      document.getElementsByTagName("body")[0].style.backgroundColor = bg;
+      document.getElementsByTagName("body")[0].style.color = font;
+      for (let i = 0; i < 3; i++)
+        document.getElementsByClassName("sp")[i].style.color = special;
+      document.getElementsByTagName("textarea")[0].style.backgroundColor =
+        textareaColor;
+      document.getElementsByTagName("textarea")[0].style.color = textareaFont;
+      document.getElementsByTagName("nav")[0].style.color = font;
+      for (let j = 0; j < 6; j++) {
+        document.getElementsByTagName("button")[j].style.backgroundColor =
+          button;
+        document.getElementsByTagName("button")[j].style.color = buttonFont;
+      }
 
-        showAlert("Theme changed", cls);
-    };
-
-    return (
-        <>
-            <Navbar
-                title="TextUtils"
-                applyTheme={applyTheme}
-            />
-            <Alert alert={alert}/>
-            <TextForm heading="Enter text to analyze below" showAlert={showAlert}/>
-
-        </>
-    );
+      document.getElementsByClassName("bg-" + cls)[0].style.boxShadow = `0 0 5px 5px ${bg}`;
+      
+      setTheme({
+        bg: button,
+        otherColor:special,
+        anotherColor: textareaColor, 
+        color:font
+        })
+      showAlert("Theme changed", cls);
+    } catch {}
+  };
+  
+  
+  return (
+    <Router>
+      <>
+        <Navbar title="TextUtils" applyTheme={applyTheme} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route exact path="/about" element={<About theme={theme}/>} />
+          <Route exact path="/"element={
+              <TextForm heading="Enter text to analyze below"showAlert={showAlert}/>}/>
+        </Routes>
+      </>
+    </Router>
+  );
 }
 
 export default App;
--- Learning-Code/project-1/src/Components/About.js
+++ Learning-Code/project-1/src/Components/About.js
@@ -1,100 +1,82 @@
 import React, { useState } from "react";
 
-export default function About() {
+export default function About(props) {
   return (
     <>
-      <div className="accordion my-5 mx-5" id="accordionExample">
-        <h2>About Us</h2>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingOne">
-            <button
-              className="accordion-button"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseOne"
-              aria-expanded="true"
-              aria-controls="collapseOne"
+      <div className="container" >
+        <div className="accordion my-5 mx-5" id="accordionExample"style={{backgroundColor: props.theme.bg, color:props.theme.color}}>
+          <h2>About Us</h2>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button
+                className="accordion-button"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseOne"
+                aria-expanded="true"
+                aria-controls="collapseOne"
+                style={{backgroundColor: props.theme.anotherColor}}
+              >
+                <strong>Analyze Your Text</strong>
+              </button>
+            </h2>
+            <div
+              id="collapseOne"
+              className="accordion-collapse collapse show"
+              aria-labelledby="headingOne"
+              data-bs-parent="#accordionExample"
             >
-              Analyze Your Text
-            </button>
-          </h2>
-          <div
-            id="collapseOne"
-            className="accordion-collapse collapse show"
-            aria-labelledby="headingOne"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body">
-              <strong>This is the first item's accordion body.</strong> It is
-              shown by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <div className="accordion-body"style={{backgroundColor: props.theme.otherColor}}>
+                TextUtils gives you a way to analyze your text quickly and efficiently. Be it word count, character count 
+              </div>
             </div>
           </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingTwo">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseTwo"
-              aria-expanded="false"
-              aria-controls="collapseTwo"
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseTwo"
+                aria-expanded="false"
+                aria-controls="collapseTwo"
+              >
+                <strong>Free to Use</strong>
+              </button>
+            </h2>
+            <div
+              id="collapseTwo"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingTwo"
+              data-bs-parent="#accordionExample"
             >
-              Free to Use
-            </button>
-          </h2>
-          <div
-            id="collapseTwo"
-            className="accordion-collapse collapse"
-            aria-labelledby="headingTwo"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body">
-              <strong>This is the second item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <div className="accordion-body">
+                TextUtils is a free character counter tool that provides instant character count and word count statistics for a given text
+              </div>
             </div>
           </div>
-        </div>
-        <div className="accordion-item">
-          <h2 className="accordion-header" id="headingThree">
-            <button
-              className="accordion-button collapsed"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#collapseThree"
-              aria-expanded="false"
-              aria-controls="collapseThree"
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button
+                className="accordion-button collapsed"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#collapseThree"
+                aria-expanded="false"
+                aria-controls="collapseThree"
+              >
+                <strong>Browser Compatible</strong>
+              </button>
+            </h2>
+            <div
+              id="collapseThree"
+              className="accordion-collapse collapse"
+              aria-labelledby="headingThree"
+              data-bs-parent="#accordionExample"
             >
-              Browser Compatible
-            </button>
-          </h2>
-          <div
-            id="collapseThree"
-            className="accordion-collapse collapse"
-            aria-labelledby="headingThree"
-            data-bs-parent="#accordionExample"
-          >
-            <div className="accordion-body">
-              <strong>This is the third item's accordion body.</strong> It is
-              hidden by default, until the collapse plugin adds the appropriate
-              classes that we use to style each element. These classes control
-              the overall appearance, as well as the showing and hiding via CSS
-              transitions. You can modify any of this with custom CSS or
-              overriding our default variables. It's also worth noting that just
-              about any HTML can go within the <code>.accordion-body</code>,
-              though the transition does limit overflow.
+              <div className="accordion-body">
+                This word counter software works in any web browser such as Chrome, Safari, Internet Explorer, Edge, Firefox and Opera. It suits to count words, characters in facebook,blog, books, document, pdf, essays, etc.
+              </div>
             </div>
           </div>
         </div>
