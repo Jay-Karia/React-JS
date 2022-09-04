b8b4685d793930ce08a9cc81eac30cc28c3f460a
Jay <jay.sanjay.karia@gmail.com>
2022-09-04 10:44:17 +0530

    navbar added

--- Learning-Code/newsapp/src/App.js
+++ Learning-Code/newsapp/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './Components/Navbar';
 
 export default class App extends Component {
   c = "Jay"
   render() {
     return (
-      <div>Hello my first class based component by: {this.c}</div>
+      <>
+        <Navbar/>
+      </>
     )
   }
 }
