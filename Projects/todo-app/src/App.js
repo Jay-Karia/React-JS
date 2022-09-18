1a80d5ed5167098fefc6038992df97db9dea3b77
Jay <jay.sanjay.karia@gmail.com>
2022-09-18 22:23:29 +0530

    progress on adding todo

--- Projects/todo-app/src/App.js
+++ Projects/todo-app/src/App.js
@@ -5,24 +5,86 @@ import Todos from "./Components/Todos";
 import { useState } from "react";
 
 function App() {
-  let storageTodos = localStorage.getItem('todo');
-  if (storageTodos===null) {
-    storageTodos = []
-  }
+    const handleAdd = () => {
+        let stTodos = JSON.parse(localStorage.getItem("todos"));
+        if (stTodos === null) {
+            stTodos = [];
+        }
+        stTodos.push([
+            {
+                title: document.getElementsByClassName("title")[0].value,
+                description: document.getElementsByClassName("desc")[0].value,
+            },
+        ]);
+        localStorage.setItem("todos", JSON.stringify(stTodos));
+        setTodos(stTodos)
+
+        document.getElementsByClassName("btn")[0].innerHTML = "✓";
+        document.getElementsByClassName("btn")[0].style.backgroundColor =
+            "#14A44D";
+        document.getElementsByClassName("title")[0].value = "";
+        document.getElementsByClassName("desc")[0].value = "";
+
+        setTimeout(() => {
+            document.getElementsByClassName("btn")[0].innerHTML = "+";
+            document.getElementsByClassName("btn")[0].style.backgroundColor =
+                "#332D2D";
+        }, 2000);
+    };
+
+    let storageTodos = localStorage.getItem("todo");
+    if (storageTodos === null) {
+        storageTodos = [];
+    }
     let [todos, setTodos] = useState(storageTodos);
-    
+
     const del = (sr_no) => {
         let deleted = todos.splice(sr_no - 1, 1);
         setTodos(todos);
-        console.log(storageTodos)
-    }
+        console.log(storageTodos);
+    };
     return (
         <>
             <Navbar />
+            <div className="container my-5">
+                <h1 className="text-center my-5">Add a Todo</h1>
+                <div className=" d-flex">
+                    <div
+                        className="input-group mb-3 mx-2 "
+                        style={{ width: "30%" }}
+                    >
+                        <input
+                            maxLength={20}
+                            type="text"
+                            className="form-control title"
+                            aria-label="Sizing example input"
+                            aria-describedby="inputGroup-sizing-default"
+                            placeholder="Todo Title"
+                        />
+                    </div>
+                    <div className="input-group mb-3 " style={{ width: "50%" }}>
+                        <textarea
+                            type="text"
+                            maxLength={74}
+                            className="form-control desc"
+                            aria-label="Sizing example input"
+                            aria-describedby="inputGroup-sizing-default"
+                            placeholder="Todo Description"
+                        />
+                    </div>
+                    <div
+                        className="btn btn-dark mx-3"
+                        style={{ height: "10%", border: "none" }}
+                        onClick={handleAdd}
+                    >
+                        +
+                    </div>
+                </div>
+            </div>
             <Todos del={del} todos={todos} />
             <Footer />
         </>
-    )
+    );
 }
 
 export default App;
--- Projects/todo-app/src/Components/Todos.js
+++ Projects/todo-app/src/Components/Todos.js
@@ -3,43 +3,10 @@ import TodoItem from "./TodoItem";
 
 export default function Todos(props) {
 
-    const handleAdd = ()=>{
-
-        let stTodos = JSON.parse(localStorage.getItem('todos'))
-        if (stTodos===null) {stTodos = []}
-        stTodos.push([
-            {
-                title: document.getElementsByClassName('title')[0].value,
-                description: document.getElementsByClassName('desc')[0].value
-            }
-        ])
-        localStorage.setItem('todos', JSON.stringify(stTodos))
-
-        document.getElementsByClassName('btn')[0].innerHTML = "✓"
-        document.getElementsByClassName('btn')[0].style.backgroundColor = "#14A44D"
-        document.getElementsByClassName('title')[0].value = ""
-        document.getElementsByClassName('desc')[0].value = ""
-        
-
-        setTimeout(() => {
-            document.getElementsByClassName('btn')[0].innerHTML = "+"
-            document.getElementsByClassName('btn')[0].style.backgroundColor = "#332D2D"
-        }, 2000);
-    }
+    
     return (
         <>
-        <div className="container my-5">
-        <h1 className="text-center my-5" >Add a Todo</h1>
-            <div className=" d-flex">
-            <div className="input-group mb-3 mx-2 "style={{width:"30%"}}>
-                <input maxLength={20} type="text" className="form-control title"  aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Todo Title"/>
-            </div>
-            <div className="input-group mb-3 "style={{width:"50%"}}>
-                <textarea type="text" maxLength={74} className="form-control desc" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" placeholder="Todo Description"/>
-            </div>
-            <div className="btn btn-dark mx-3" style={{height: "10%", border:"none"}} onClick={handleAdd}>+</div>
-            </div>
-        </div>
+        
         <hr />
             <div className="container my-4">
             <h1 className="text-center">Your Todos List</h1>
