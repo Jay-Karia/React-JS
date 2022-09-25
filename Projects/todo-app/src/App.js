b8502780e391c253f82b13e9d4d083de6ae424c3
Jay <jay.sanjay.karia@gmail.com>
2022-09-25 12:34:10 +0530

    refresh page todo disappear bug solved

--- Projects/todo-app/src/App.js
+++ Projects/todo-app/src/App.js
@@ -5,13 +5,16 @@ import Todos from "./Components/Todos";
 import { useState } from "react";
 
 function App() {
-
-    let storageTodos = JSON.parse(localStorage.getItem("todo"));
+    let storageTodos = JSON.parse(localStorage.getItem("todos"));
     if (storageTodos === null) {
         storageTodos = [];
     }
     
     let [todos, setTodos] = useState(storageTodos);
+    const [len, setLen] = useState(storageTodos.length)
+    let len2 = storageTodos.length
+    // console.log(len);
+
 
     const handleAdd = () => {
         let stTodos = JSON.parse(localStorage.getItem("todos"));
@@ -28,6 +31,7 @@ function App() {
         ]);
         localStorage.setItem("todos", JSON.stringify(stTodos));
         setTodos(stTodos)
+        window.length = stTodos.length
 
         document.getElementsByClassName('desc')[0].style.backgroundColor = 'white'
         document.getElementsByClassName('title')[0].style.backgroundColor = 'white'
@@ -45,17 +49,24 @@ function App() {
                 document.getElementsByClassName("wd1")[0].innerHTML = 20
                 document.getElementsByClassName("wd")[0].innerHTML = 75
         }, 1000);
-    };
+    }
 
     const handleDone = (id)=>{
         let stTodos = JSON.parse(localStorage.getItem("todos"));
         if (stTodos === null) {
             stTodos = [];
         }
+        
         if (stTodos[id][0].completed===true) {
             stTodos[id][0].completed = false
+            // len = len+1
+            setLen(len2+1)
+            // console.log(len);
         } else if (stTodos[id][0].completed===false) {
             stTodos[id][0].completed = true
+            // len = len-1
+            setLen(len2-1)
+            // console.log(len);
         }
         localStorage.setItem("todos", JSON.stringify(stTodos));
         setTodos(stTodos)
@@ -144,7 +155,7 @@ function App() {
                     </div>
                 </div>
             </div>
-            <Todos del={handleDelete} todos={todos} done={handleDone} edit={handleEdit}/>
+            <Todos del={handleDelete} todos={todos} done={handleDone} edit={handleEdit} len={len}/>
             <Footer />
         </>
     );
--- Projects/todo-app/src/Components/Todos.js
+++ Projects/todo-app/src/Components/Todos.js
@@ -9,7 +9,7 @@ export default function Todos(props) {
         <hr />
             <div className="container my-4">
             <h1 className="text-center">Your Todos List</h1>
-            {props.todos.length!==0?<h3 className="text-center my-5"><strong className="text-success">{props.todos.length}</strong> {props.todos.length>1?"todos":"todo"} remaining</h3>:""}
+            {props.todos.length!==0?<h3 className="text-center my-5"><strong className="text-success">{props.len}</strong> {props.len>1?"todos":"todo"} remaining</h3>:""}
             
                 {props.todos.length===0?<h5 className="text-center text-danger my-5">No Todos to Display</h5>: props.todos.map((todo) => {
                     return (
