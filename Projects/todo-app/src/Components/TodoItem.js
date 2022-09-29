d0abb49c1e058b7fda2021674c9ade1587bb8e63
Jay <jay.sanjay.karia@gmail.com>
2022-09-29 21:42:31 +0530

    due date fully done

--- Projects/todo-app/src/Components/TodoItem.js
+++ Projects/todo-app/src/Components/TodoItem.js
@@ -6,19 +6,7 @@ export default function TodoItem(props) {
             <div className="container my-5">
                 
                     {props.todo.map((e, i)=>{
-                        /*  <div className="d-flex" style={{width:'120%'}}>*/
                         return <>
-                        {/* <div className='item' style={{backgroundColor: props.todo[i].completed===true?'hsl(150, 60%, 85%)':'white'}} >
-                        <strong className="todoTitle">{props.todo[i].title}</strong> <span className="todoDesc">{props.todo[i].description}</span>
-                            <div style={{float:'right'}}>
-                                <img onClick={()=>{props.edit(props.todo[i].key)}} src={require('../edit.jpg')} alt="edit" style={{marginRight:'20px'}} />
-                                <img onClick={()=>{props.done(props.todo[i].key)}} src={require('../done.jpg')} alt="done" style={{marginRight:'20px'}} />
-                                <img onClick={()=>{props.del(props.todo[i].key)}} src={require('../del.png')} alt="delete" />
-                            </div>
-                        </div>
-                        <div className="text-center my-2" style={{fontWeight:'bolder', fontSize:'1.2rem', color:props.todo[i].due.split('-')[2]-String(new Date().getDate()).padStart(2, '0')>10?'#5cb85c':'orangered'}}>
-                            • {props.todo[i].due.split('-')[2]-String(new Date().getDate()).padStart(2, '0')==="NaN"?"Due "+props.todo[i].due.split('-')[2]-String(new Date().getDate()).padStart(2, '0')+'d':"No Due Date"}
-                        </div> */}
                         <div className="item card text-center">
                             <div className="card-header" style={{backgroundColor: props.todo[i].completed===true?'hsl(150, 60%, 85%)':'white'}}>
                                 <strong className="todoTitle">{props.todo[i].title}</strong>
@@ -27,10 +15,10 @@ export default function TodoItem(props) {
                                 <p className="card-text">{props.todo[i].description}</p>
                             </div>
                             <hr />
-                            <div className="my-2" style={{color: Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))>5?"green":"orangered"}}>
+                            <div className="my-2 d2" style={{color: Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))>5 || props.todo[i].completed===true?"green":"#CC0000"}}>
                             <strong className="dueDate">
-                            {props.todo[i].completed===false?Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))<=0?"• Due today":"• Due "+Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24)+1)+'d':"Done"}
-
+                            {/* {props.todo[i].completed===false?Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))===0?"• Due today":"• Due "+Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))+'d':"Done"} */}
+                            { isNaN(Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24)))===true||Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))<0 ?"• No due date": props.todo[i].completed===false?Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))===0?"• Due today":"• Due "+Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))+'d':"Done"}
                             </strong>
                             
                             </div>
@@ -47,3 +35,5 @@ export default function TodoItem(props) {
         </>
     )
 }
+
+// Math.round((new Date(props.todo[i].due).getTime()-new Date().getTime())/(1000*60*60*24))
