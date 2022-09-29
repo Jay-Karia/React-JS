1b27d3b3ac085f97fe55cf6a51b9f063c9f81a5e
Jay <jay.sanjay.karia@gmail.com>
2022-09-29 21:58:34 +0530

    category drop down added, changed required

--- Projects/todo-app/src/Components/AddTodo.js
+++ Projects/todo-app/src/Components/AddTodo.js
@@ -81,6 +81,23 @@ export default function AddTodo(props) {
                             aria-label="Sizing example input"
                             aria-describedby="inputGroup-sizing-default"
                         />
+                         <div
+                        className="btn btn-dark"
+                        style={{ height: 'auto', border: "none"}}
+                        // onClick={props.handleAdd}
+                    >
+                        +
+                    </div>
+                        
+                    <div>
+                    <select name="category" id="category" className="form-control mx-2" aria-label="Default select example">
+                            <option value="1" style={{backgroundColor: '#ffc107'}}>Work</option>
+                            <option value="2" style={{backgroundColor: '#198754', color:'white'}}>Entertainment</option>
+                            <option value="3" style={{backgroundColor: '#0d6efd', color:'white'}}>Travel</option>
+                            <option value="4" style={{backgroundColor: 'orangered', color:'white'}}>Food</option>
+                            <option value="5" style={{backgroundColor: '#dc3545', color:'white'}}>Urgent</option>
+                        </select>
+                    </div>
                     </div>
                     <div
                         className="btn addBTN btn-dark"
@@ -90,12 +107,6 @@ export default function AddTodo(props) {
                         +
                     </div>
                 </div>
-                {/* <div
-                    className="text-center btn wd my-2 collapse_btn "
-                    onClick={props.toggleCollapse}
-                    style={{width: "3%",margin: "0px"}}>
-                    <strong className="text-center">⬆</strong>
-                </div> */}
             </div>
         </>
     );
