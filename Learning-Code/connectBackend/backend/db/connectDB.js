9277f818b3b5b449b82cf41b14537945d235c8c2
Jay <jay.sanjay.karia@gmail.com>
2022-10-16 20:23:30 +0530

    db connection error

--- Learning-Code/connectBackend/backend/db/connectDB.js
+++ Learning-Code/connectBackend/backend/db/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 function connect() {
-    const db_uri = 'mongodb+srv://Jay:<jskAuthTest>@cluster0.s0b7o1p.mongodb.net/?retryWrites=true&w=majority'
+    const db_uri = 'mongodb+srv://JK:<09Jan1973>@cluster0.s0b7o1p.mongodb.net/?retryWrites=true&w=majority'
     mongoose.connect(db_uri)
     console.log('DB connected');
 }
