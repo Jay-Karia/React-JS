f8dbcece30b02c7d9b3b27bccf1da1e8c5ad88d4
Jay <jay.sanjay.karia@gmail.com>
2022-10-18 12:03:45 +0530

    express validation problems

--- Learning-Code/connectBackend/backend/Routes/auth.js
+++ Learning-Code/connectBackend/backend/Routes/auth.js
@@ -1,16 +1,37 @@
 const express = require('express')
 const router = express.Router()
+const { body, validationResult } = require('express-validator')
+
+var bcrpytjs = require('bcryptjs')
 
 const User = require('../Models/user')
 
-router.post('/createUser', (req, res) => {
-    const users = new User({
-        "Name": "Jay Sanjay Karia",
-        "Email": "jay.sanjay.karia@gmail.com",
-        "Password": "09Jan197#"
+router.get('/createUser', body('Name').isLength({ min: 3 }), body('Email').isEmail(), (req, res) => {
+    let salt = ''
+    let secPass = ''
+    async function genSecPass() {
+        globalThis.secPass
+        salt = await bcrpytjs.genSalt(10)
+        secPass = await bcrpytjs.hash("09Jan197#", salt)
+        return secPass
+    }
+    genSecPass().then((hash) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log(errors.array())
+        }
+        const users = new User({
+            Name: "jays",
+            Email: "jay.sanjay.karia@gmail.com",
+            Password: hash
+        })
+        users.save()
+        res.send('New user created')
+    }).catch((err) => {
+        console.log(err)
+        res.send('Could not create user')
     })
-    users.save()
-    res.send('New user created')
 })
 
 module.exports = router
