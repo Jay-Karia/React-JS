ef06c64f0febe292926f75c23c1f9438cec53240
Jay <jay.sanjay.karia@gmail.com>
2022-10-19 10:53:16 +0530

    errors in recieving data

--- Learning-Code/connectBackend/backend/App.js
+++ Learning-Code/connectBackend/backend/App.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const App = express()
+const app = express()
 const routes = require('./Routes/routes')
-const auth = require('./Routes/auth')
+
+const cors = require('cors')
 
 const connectDB = require('./db/connectDB')
 
@@ -9,10 +10,9 @@ const port = process.env.PORT || 8000
 
 connectDB()
 
-App.use(express.json())
-App.use('/api', routes)
-App.use('/api/auth', auth)
-
+app.use(express.json())
+app.use('/api', routes)
+app.use(cors())
 
 // Listening
-App.listen(port, () => console.log(`App listening on port ${port}`))
+app.listen(port, () => console.log(`App listening on port ${port}`))
--- Learning-Code/connectBackend/backend/Routes/routes.js
+++ Learning-Code/connectBackend/backend/Routes/routes.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../Models/user')
+
 router.get('/', (req, res) => {
     res.send('Hello from back')
+})
+
+router.post('/register', async(req, res) => {
+    console.log(req.body)
+    try {
+        const user = await User.create(req.body)
+            // user.save()
+        res.send(user)
+    } catch {}
 })
 
 module.exports = router
