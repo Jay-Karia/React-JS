f8485cb072596f942f0581f8c1ad309054b7895f
Jay <jay.sanjay.karia@gmail.com>
2022-10-27 11:05:49 +0530

    Delete Learning-Code/connectBackend directory

--- Learning-Code/connectBackend/backend/db/connectDB.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-function connect() {
-    const db_uri = 'mongodb+srv://Jay:09Jan1973@cluster0.vpjxkmo.mongodb.net/?retryWrites=true&w=majority'
-    mongoose.connect(db_uri)
-    console.log('DB connected');
-}
-
-module.exports = connect
