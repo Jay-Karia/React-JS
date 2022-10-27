6640d055429f491dcbe7e25170f44275863f1538
Jay <jay.sanjay.karia@gmail.com>
2022-10-27 11:05:31 +0530

    Delete Learning-Code/MERN Auth/server directory

--- Learning-Code/MERN Auth/server/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const userSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    passwordHash: { type: String, required: true }
-})
-
-const user = mongoose.model('user', userSchema)
-module.exports = user
