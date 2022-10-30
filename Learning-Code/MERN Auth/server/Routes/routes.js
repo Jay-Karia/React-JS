dcc4565f874576c102129e512ba8029a92afdff6
Jay <jay.sanjay.karia@gmail.com>
2022-10-30 17:01:10 +0530

    update

--- Learning-Code/MERN Auth/server/Routes/routes.js
+++ Learning-Code/MERN Auth/server/Routes/routes.js
@@ -30,19 +30,26 @@ router.post('/register', async(req, res) => {
 
 // Verification of JWT
 function verifyJWT(req, res, next) {
-    const token = req.headers['x-access-token'].split(' ')[1]
-    if (token) {
-        jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
-            if (err) return res.json({
-                isLoggedIn: false,
-                msg: 'Failed to Authenticate'
+    try {
+        const token = req.headers['x-access-token'].split(' ')[1]
+        if (token) {
+            jwt.verify(token, process.env.PASSPORTSECRET, (err, decoded) => {
+                if (err) return res.json({
+                    isLoggedIn: false,
+                    msg: 'Failed to Authenticate'
+                })
+                req.user = {}
+                req.user.id = decoded.is
+                req.user.name = decoded.name
+                next()
             })
-            req.user = {}
-            req.user.id = decoded.is
-            req.user.name = decoded.name
-            next()
-        })
-    } else {
+        } else {
+            res.status(400).json({
+                msg: 'Incorrect Token given',
+                status: "error"
+            })
+        }
+    } catch {
         res.status(400).json({
             msg: 'Incorrect Token given',
             status: "error"
